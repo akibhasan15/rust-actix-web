ea9155fb3ccaf18db2cf51b483e35cff8b4608ee
Akib Hasan <42295643+akibhasan15@users.noreply.github.com>
2024-05-15 13:48:51 +0600

    Add files via upload

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+use actix_web::{web, App, HttpServer, Responder};
+
+async fn index() -> impl Responder {
+    "Hello, World!"
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(index))
+    })
+    .bind("127.0.0.1:4000")?
+    .run()
+    .await
+}
+
